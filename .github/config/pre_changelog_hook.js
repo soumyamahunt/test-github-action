7f62d05a44a011dd5928de84d7b0b08f9956d779
Soumya Mahunt <soumya.mahunt@tatadigital.com>
2022-03-08 22:15:15 +0530

    docs: add skip version file

--- .github/config/pre_changelog_hook.js
+++ .github/config/pre_changelog_hook.js
@@ -1,19 +1,20 @@
 const path = require('path');
 const fs = require('fs');
 const semver = require('semver')
+const core = require('@actions/core')
 
 exports.preVersionGeneration = (version) => {
   const { GITHUB_WORKSPACE } = process.env;
-  console.log(`version: ${version}`);
+  core.info(`version: ${version}`);
   const gem_info_file = path.join(GITHUB_WORKSPACE, 'lib/cocoapods-embed-flutter/gem_version.rb');
   const gem_info = fs.readFileSync(gem_info_file);
-  console.log(`gem_info: ${gem_info}`);
+  core.info(`gem_info: ${gem_info}`);
   currentVersion = gem_info.match(/VERSION\s*=\s'(.*)'/g)[1];
-  console.log(`currentVersion: ${currentVersion}`);
+  core.info(`currentVersion: ${currentVersion}`);
   if (semver.lt(version, currentVersion)) { version = currentVersion; }
-  console.log(`version: ${version}`);
+  core.info(`version: ${version}`);
   const new_gem_info = gem_info.replace(/VERSION\s*=\s*.*/g, `VERSION = '${version}'`);
-  console.log(`new_gem_info: ${new_gem_info}`);
+  core.info(`new_gem_info: ${new_gem_info}`);
   fs.writeFileSync(gem_info_file, new_gem_info);
   return version;
 }
