87b1419a48bbf7ec663293f784514974d90d2f36
Soumya Mahunt <soumya.mahunt@tatadigital.com>
2022-03-08 22:43:17 +0530

    ci: fix empty version

--- .github/config/pre_changelog_hook.js
+++ .github/config/pre_changelog_hook.js
@@ -9,7 +9,7 @@ exports.preVersionGeneration = (version) => {
   const gem_info_file = path.join(GITHUB_WORKSPACE, 'lib/cocoapods-embed-flutter/gem_version.rb');
   const gem_info = `${fs.readFileSync(gem_info_file)}`;
   core.info(`gem_info: ${gem_info}`);
-  currentVersion = gem_info.match(/VERSION\s*=\s'(.*)'/g)[1];
+  currentVersion = gem_info.match(/VERSION\s*=\s'(.*)'/)[1];
   core.info(`currentVersion: ${currentVersion}`);
   if (semver.lt(version, currentVersion)) { version = currentVersion; }
   core.info(`version: ${version}`);
